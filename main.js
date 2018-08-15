e54f30ec73a5cb2b25b2970e0a066d347434fa5b
lacehileman <35512856+lacehileman@users.noreply.github.com>
2018-08-14 22:24:51 -0500

    VectorTileLayer
    Vector Tile Layer Lesson
    index.html:
    set initial page and style
    main.js:
    sets up initial map with use of data source from openmaptiles.com

---
+++ main.js
@@ -0,0 +1,29 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import MVT from 'ol/format/MVT';
+import VectorTileLayer from 'ol/layer/VectorTile';
+import VectorTileSource from 'ol/source/VectorTile';
+
+const key = 'QrA7Q760PLWUIx8duHKP';//geocoding key: Fgz5zUrzX2dG2bXkleCU ; map key: QrA7Q760PLWUIx8duHKP
+
+const map = new Map({
+    target: 'map-container',
+    view: new View({
+        center: [0, 0],
+        zoom: 2
+    })
+});
+
+const layer = new VectorTileLayer({ 
+    source: new VectorTileSource({ 
+        attributions: [ 
+            '<a href="http://www.openmaptiles.org/" target="_blank">&copy; OpenMapTiles</a>', 
+            '<a href="http://www.openstreetmap.org/about/" target="_blank">&copy; OpenStreetMap contributors</a>' 
+        ], 
+        format: new MVT(), 
+        url: `https://free-{1-3}.tilehosting.com/data/v3/{z}/{x}/{y}.pbf.pict?key=${key}`, 
+        maxZoom: 14 
+    }) 
+}); 
+map.addLayer(layer); 
